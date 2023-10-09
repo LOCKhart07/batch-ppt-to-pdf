e15dcee418ab3f3dbd79c636bc527711fca9d867
Jenslee Dsouza <33950242+LOCKhart07@users.noreply.github.com>
2023-10-09 12:13:39 +0530

    Update batch_ppt_to_pdf.py

--- batch_ppt_to_pdf.py
+++ batch_ppt_to_pdf.py
@@ -1,24 +1,41 @@
 import comtypes.client
 import os
 
+
 def init_powerpoint():
     powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
     powerpoint.Visible = 1
     return powerpoint
 
-def ppt_to_pdf(powerpoint, inputFileName, outputFileName, formatType = 32):
-    if outputFileName[-3:] != 'pdf':
+
+def ppt_to_pdf(powerpoint, inputFileName, outputFileName, formatType=32):
+    if outputFileName[-3:] != "pdf":
         outputFileName = outputFileName + ".pdf"
     deck = powerpoint.Presentations.Open(inputFileName)
-    deck.SaveAs(outputFileName, formatType) # formatType = 32 for ppt to pdf
+    deck.SaveAs(outputFileName, formatType)  # formatType = 32 for ppt to pdf
     deck.Close()
+
 
 def convert_files_in_folder(powerpoint, folder):
     files = os.listdir(folder)
     pptfiles = [f for f in files if f.endswith((".ppt", ".pptx"))]
+
+    failed = []
+
+    counter = 1
     for pptfile in pptfiles:
+        print(f"{counter}/{len(pptfiles)}    : {pptfile}")
         fullpath = os.path.join(folder, pptfile)
-        ppt_to_pdf(powerpoint, fullpath, fullpath)
+        try:
+            ppt_to_pdf(powerpoint, fullpath, fullpath)
+        except Exception as e:
+            print(e)
+            failed.append(pptfile)
+
+        counter += 1
+
+    print("List of failed files: {failed}")
+
 
 if __name__ == "__main__":
     powerpoint = init_powerpoint()
